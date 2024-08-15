47ab157e22e799708436735de0609539c51695c2
Gulnara <gulnaranocetti@gmail.com>
2024-08-15 20:31:09 -0300

    cambios

--- evento.js
+++ evento.js
@@ -1,17 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
-    //Selecciona el div usando su id
-    const divContent = document.getElementById('content-button');
+//Selecciona el div usando su id
+const divContent = document.getElementById('content-button');
 
-    //anadir el manejador de eventos para el click
-    divContent.addEventListener('click', function(){
-        alert('Hola! Soy el div');
-    })
-
-    // Seleccionar el botón usando su id
-    const boton = document.getElementById('miBoton');
-    
-    // Añadir el manejador de eventos para el clic en el botón
-    boton.addEventListener('click', (event) => {
-        event.stopPropagation(); // Evitar que el evento se propague al div
-    });
+//anadir el manejador de eventos para el click
+divContent.addEventListener('click', function(){
+    alert('Hola! Soy el div');
 })
+
+// Seleccionar el botón usando su id
+const boton = document.getElementById('miBoton');
+
+// Añadir el manejador de eventos para el clic en el botón
+boton.addEventListener('click', (event) => {
+    event.stopPropagation(); // Evitar que el evento se propague al div
+});
